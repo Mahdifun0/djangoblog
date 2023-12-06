f225cae0ee3429f939c595151c811880242d5fe9
Mahdi <m09371840596mr@gmail.com>
2023-12-06 13:54:48 +0330

    end of this course

--- articles/views.py
+++ articles/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from . import models
 
 
 def articleslist(request):
-    articles = models.Article.objects.all().order_by("date")
+    articles = models.Article.objects.all().order_by("-date")
 
     args = {"articles": articles}
 
     return render(request, "articles/articleslist.html", args)
+
+
+def article_detail(request, slug):
+    article = models.Article.objects.get(slug=slug)
+    return render(request, "articles/article_detail.html", {"article": article})
